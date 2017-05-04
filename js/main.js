9f89e18e775f93b4d04fb13ce80182a965210fc6
Krzysztof Burkot <buryPL@gmail.com>
2017-05-04 17:30:14 +0200

    Bootstrap_header_services_styles
    Style na header i services done, init na portfolio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+/*SMOOTH SCROLL*/
+$(document).ready(function () {
+    $('.menu_list_items').on('click', function (e) {
+        e.preventDefault();     
+        $('html, body').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 500);
+    });
+});
+
+/*MENU FONT CHANGES WITH SCROLL*/
